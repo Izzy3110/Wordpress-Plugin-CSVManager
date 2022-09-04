b807e7db52c9de1a5389b127c89f955d640cc035
Sascha Frank <sascha@tabteam.media>
2022-09-04 13:21:28 +0200

    tmp

--- csvmanager/csvmanager.php
+++ csvmanager/csvmanager.php
@@ -30,15 +30,20 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-add_action('admin_post_upload_csv', 'my_callback_fn');
-function my_callback_fn($data) {
-	header("Location: admin.php?page=csvmanager%2Fcsvmanager.php&action=upload&result=success");
-}
 
+
+
+
+function aad_render_admin() {
+	?>
+	<h1>ADMIN</h1>
+	<?php
+}
 // create custom plugin settings menu
 add_action('admin_menu', 'csv_manager_plugin_create_menu');
 
 function csv_manager_plugin_create_menu() {
+	//add_options_page(__('Admin Ajax Demo', 'aa'), __('Admin Ajax', 'aad'), 'manage_options', 'admin-ajax-demo', 'aad_render_admin');
     //create new top-level menu
     add_menu_page('CSV Manager', 'CSV Manager', 'administrator', __FILE__, 'csv_manager_plugin_main_page' , plugins_url('/images/favicon_index_32_16x16_1.png', __FILE__) );
 
@@ -107,6 +112,15 @@ function get_link_url($entry) {
 }
 
 function csv_manager_plugin_main_page() {
+	
+	add_options_page(
+            'Settings Admin', 
+            'My Settings', 
+            'manage_options', 
+            'my-setting-admin', 
+            'aad_render_admin'
+        );
+	
     ?>
     <div class="wrap">
 	<h1>CSV Manager</h1>
@@ -321,177 +335,3 @@ function wpdocs_enqueue_custom_admin_style() {
         wp_enqueue_style( 'custom_wp_admin_css' ); 
 }
 add_action( 'admin_enqueue_scripts', 'wpdocs_enqueue_custom_admin_style' ); 
-
-
-
-function csv_manager_plugin_settings_page() {
-    ?>
-    <div class="wrap">
-    <h1>Your Plugin Name</h1>
-
-    <form method="post" action="options.php">
-        <?php settings_fields( 'csv-manager-plugin-settings-group' ); ?>
-        <?php do_settings_sections( 'csv-manager-plugin-settings-group' ); ?>
-        <table class="form-table">
-            <tr valign="top">
-            <th scope="row">New Option Name</th>
-            <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
-            </tr>
-
-            <tr valign="top">
-            <th scope="row">Some Other Option</th>
-            <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
-            </tr>
-
-            <tr valign="top">
-            <th scope="row">Options, Etc.</th>
-            <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
-            </tr>
-        </table>
-
-        <?php submit_button(); ?>
-
-    </form>
-    </div>
-    <?php
-}
-
-
-class MySettingsPage {
-    /**
-     * Holds the values to be used in the fields callbacks
-     */
-    private $options;
-
-    /**
-     * Start up
-     */
-    public function __construct()
-    {
-        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
-        add_action( 'admin_init', array( $this, 'page_init' ) );
-    }
-
-    /**
-     * Add options page
-     */
-    public function add_plugin_page()
-    {
-        // This page will be under "Settings"
-        add_options_page(
-            'Settings Admin', 
-            'CSV Manager', 
-            'manage_options', 
-            'my-setting-admin', 
-            array( $this, 'create_admin_page' )
-        );
-    }
-
-    /**
-     * Options page callback
-     */
-    public function create_admin_page()
-    {
-        // Set class property
-        $this->options = get_option( 'my_option_name' );
-        ?>
-        <div class="wrap">
-            <h1>My Settings</h1>
-            <form method="post" action="options.php">
-            <?php
-                // This prints out all hidden setting fields
-                settings_fields( 'my_option_group' );
-                do_settings_sections( 'my-setting-admin' );
-                submit_button();
-            ?>
-            </form>
-        </div>
-        <?php
-    }
-
-    /**
-     * Register and add settings
-     */
-    public function page_init()
-    {        
-        register_setting(
-            'my_option_group', // Option group
-            'my_option_name', // Option name
-            array( $this, 'sanitize' ) // Sanitize
-        );
-
-        add_settings_section(
-            'setting_section_id', // ID
-            'My Custom Settings', // Title
-            array( $this, 'print_section_info' ), // Callback
-            'my-setting-admin' // Page
-        );  
-
-        add_settings_field(
-            'id_number', // ID
-            'ID Number', // Title 
-            array( $this, 'id_number_callback' ), // Callback
-            'my-setting-admin', // Page
-            'setting_section_id' // Section           
-        );      
-
-        add_settings_field(
-            'title', 
-            'Title', 
-            array( $this, 'title_callback' ), 
-            'my-setting-admin', 
-            'setting_section_id'
-        );      
-    }
-
-    /**
-     * Sanitize each setting field as needed
-     *
-     * @param array $input Contains all settings fields as array keys
-     */
-    public function sanitize( $input )
-    {
-        $new_input = array();
-        if( isset( $input['id_number'] ) )
-            $new_input['id_number'] = absint( $input['id_number'] );
-
-        if( isset( $input['title'] ) )
-            $new_input['title'] = sanitize_text_field( $input['title'] );
-
-        return $new_input;
-    }
-
-    /** 
-     * Print the Section text
-     */
-    public function print_section_info()
-    {
-        print 'Enter your settings below:';
-    }
-
-    /** 
-     * Get the settings option array and print one of its values
-     */
-    public function id_number_callback()
-    {
-        printf(
-            '<input type="text" id="id_number" name="my_option_name[id_number]" value="%s" />',
-            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
-        );
-    }
-
-    /** 
-     * Get the settings option array and print one of its values
-     */
-    public function title_callback()
-    {
-        printf(
-            '<input type="text" id="title" name="my_option_name[title]" value="%s" />',
-            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
-        );
-    }
-}
-
-if( is_admin() ) {
-    $my_settings_page = new MySettingsPage();
-}
